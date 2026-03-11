b6a61f96409c1d57996d82c99b486cb75c201415
Joh <j.gleize792@gmail.com>
2026-03-11 17:29:29 +0100

    Move all files to app/ and app/api to match a API file structure type

---
+++ server/app/services/llm_service.py
@@ -0,0 +1,42 @@
+import httpx
+from app.core.config import settings
+
+class LLMService:
+    def __init__(self):
+        self.base_url = (
+            f"https://api.infomaniak.com/1/ai/{settings.INFOMANIAK_PRODUCT_ID}/openai/chat/completions"
+        )
+        self.headers = {
+            "Authorization": f"Bearer {settings.INFOMANIAK_API_KEY}",
+            "Content-Type": "application/json"
+        }
+
+    async def generate_response(self, user_prompt: str) -> str:
+        """
+        Sent async request to Atlas' Infomaniak API
+        """
+        data = {
+            "model": settings.INFOMANIAK_MODEL,
+            "messages": [
+                {"role": "system", "content": settings.ATLAS_SYSTEM_PROMPT},
+                {"role": "user", "content": user_prompt}
+            ],
+            "temperature": 0.7,
+            "max_tokens": 50
+        }
+
+        async with httpx.AsyncClient(timeout=30.0) as client:
+            try:
+                response = await client.post(self.base_url, headers=self.headers, json=data)
+                
+                if response.status_code != 200:
+                    return f"API error : ({response.status_code}): {response.text}"
+                
+                result = response.json()
+                return result['choices'][0]['message']['content'].strip()
+                
+            except httpx.HTTPError as e:
+                return f"Communication error with the LLM: {str(e)}"
+
+# On instancie un singleton pour le service
+llm_service = LLMService()
