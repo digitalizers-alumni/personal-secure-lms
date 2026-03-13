2184191de8c5723e82041fff9d7e2ceb77b5793e
Joh <j.gleize792@gmail.com>
2026-03-13 16:08:38 +0100

    Refactore document_schema.py

--- server/app/models/document_schema.py
+++ server/app/models/document_schema.py
@@ -1,10 +1,6 @@
-# Ce fichier sert de passerelle pour la compatibilité avec le code existant
-# Il regroupe les modèles définis dans des fichiers distincts.
-
 from .users import User, UserRole
 from .documents import Document
 from .courses import Course
-
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from app.db.database import Base
