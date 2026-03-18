f68f8bcd6284778979e978fa5714f7f4409d60f2
Joh <j.gleize792@gmail.com>
2026-03-18 07:31:54 +0100

    Added npm run dev:cloud to request cloud infra

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://192.168.78.133:8000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       }
     }
